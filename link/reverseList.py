ff41c6a50b403104808b5eece997000b8f834080
ouzm <wjj315315@gmail.com>
2019-11-22 22:35:01 +0800

    init & link reverse

---
+++ link/reverseList.py
@@ -0,0 +1,17 @@
+# leetcode No.206
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+        
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre = None
+        cur = head
+        while cur != None:
+            tmp = cur.next
+            pre = cur
+            cur = tmp
+
+        return pre

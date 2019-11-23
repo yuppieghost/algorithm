a6413f47e6ca46e3e9c58d4b433457639d649393
wsl <123>
2019-11-23 15:47:34 +0800

    push

---
+++ link/removeNthFromEnd.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        length = 0
+        cur = head
+        while cur:
+            length+=1
+            cur = cur.next
+        length -= n
+        cur = dummy
+        while length > 0:
+            length -= 1
+            cur = cur.next
+        cur.next = cur.next.next
+        return dummy.next
+        

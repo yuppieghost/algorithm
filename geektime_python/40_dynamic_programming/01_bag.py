8955c4ccc540b7be4db88eef6d35699a925ce013
wujj <wujj@cpchain.io>
2019-12-25 13:59:56 +0800

    01 bag  一维数组复习

--- geektime_python/40_dynamic_programming/01_bag.py
+++ geektime_python/40_dynamic_programming/01_bag.py
@@ -20,36 +20,67 @@ def bag(items_info: List[int], capacity: int) -> int:
     https://i.loli.net/2019/12/16/ipTJFZVQNrax1hb.png
 
     """
+    print(items_info)
+    n = len(items_info)
     dp = [True] + [False] * capacity
-    for i in items_info:
-        dp = [dp[c] or (c >= i and dp[c - i]) for c in range(capacity + 1)]
+    if items_info[0] <= capacity:
+        dp[items_info[0]] = True
+    for i in range(1, n):
+        for j in range(capacity - items_info[i], -1, -1):
+            if dp[j]:
+                dp[j + items_info[i]] = True
     res = [i for i, c in enumerate(dp) if c == True][-1]
     return res
-    # # 扩展成二维数组的解法
-    # # 创建 memo, (capacity + 1) * n 个 -1
-    # memo = [[-1] * (capacity + 1) for _ in range(n)]
-    # # 什么都不选
-    # memo[0][0] = 1
-    # if items_info[0] <= capacity:
-    #     # 选择第一个
-    #     # items_info[0] 即第一个物品的重量
-    #     memo[0][items_info[0]] = 1
-    #
-    # # 第0个物品决策完后的状态记录完毕
-    #
-    # for i in range(1, n):
-    #     for cur_weight in range(capacity + 1):
-    #         # 上一行存在
-    #         if memo[i - 1][cur_weight] != -1:
-    #             memo[i][cur_weight] = memo[i - 1][cur_weight]  # 不选
-    #             # 注意选择的前提条件,当前背包重量加上该物品后的总重量必须小于等于背包容量
-    #             if cur_weight + items_info[i] <= capacity:  # 选
-    #                 memo[i][cur_weight + items_info[i]] = 1
-    #
-    # # 选择最后一次状态的最大值
-    # for w in range(capacity, -1, -1):
-    #     if memo[-1][w] != -1:
-    #         return w
+
+
+# public static int knapsack2(int[] items, int n, int w) {
+#   boolean[] states = new boolean[w+1]; // 默认值false
+#   states[0] = true;  // 第一行的数据要特殊处理，可以利用哨兵优化
+#   if (items[0] <= w) {
+#     states[items[0]] = true;
+#   }
+#   for (int i = 1; i < n; ++i) { // 动态规划
+#     for (int j = w-items[i]; j >= 0; --j) {//把第i个物品放入背包
+#       if (states[j]==true) states[j+items[i]] = true;
+#     }
+#   }
+#   for (int i = w; i >= 0; --i) { // 输出结果
+#     if (states[i] == true) return i;
+#   }
+#   return 0;
+# }
+
+
+# dp = [True] + [False] * capacity
+# for i in items_info:
+#     dp = [dp[c] or (c >= i and dp[c - i]) for c in range(capacity + 1)]
+# res = [i for i, c in enumerate(dp) if c == True][-1]
+# return res
+# # 扩展成二维数组的解法
+# # 创建 memo, (capacity + 1) * n 个 -1
+# memo = [[-1] * (capacity + 1) for _ in range(n)]
+# # 什么都不选
+# memo[0][0] = 1
+# if items_info[0] <= capacity:
+#     # 选择第一个
+#     # items_info[0] 即第一个物品的重量
+#     memo[0][items_info[0]] = 1
+#
+# # 第0个物品决策完后的状态记录完毕
+#
+# for i in range(1, n):
+#     for cur_weight in range(capacity + 1):
+#         # 上一行存在
+#         if memo[i - 1][cur_weight] != -1:
+#             memo[i][cur_weight] = memo[i - 1][cur_weight]  # 不选
+#             # 注意选择的前提条件,当前背包重量加上该物品后的总重量必须小于等于背包容量
+#             if cur_weight + items_info[i] <= capacity:  # 选
+#                 memo[i][cur_weight + items_info[i]] = 1
+#
+# # 选择最后一次状态的最大值
+# for w in range(capacity, -1, -1):
+#     if memo[-1][w] != -1:
+#         return w
 
 
 def bag_with_max_value(items_info: List[Tuple[int, int]], capacity: int) -> int:
@@ -76,13 +107,15 @@ def bag_with_max_value(items_info: List[Tuple[int, int]], capacity: int) -> int:
                 memo[i][cur_weight] = memo[i - 1][cur_weight]  # 不选
                 if cur_weight + items_info[i][0] <= capacity:
                     # 选择并保留最大value ,比较当前位置**本来的值**跟新值
-                    memo[i][cur_weight + items_info[i][0]] = max(memo[i][cur_weight + items_info[i][0]],
-                                                                 memo[i - 1][cur_weight] + items_info[i][1])
+                    memo[i][cur_weight + items_info[i][0]] = max(
+                        memo[i][cur_weight + items_info[i][0]],
+                        memo[i - 1][cur_weight] + items_info[i][1],
+                    )
 
     return max(memo[-1])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # [weight, ...]
     items_info = [2, 2, 4, 6, 3]
     capacity = 9

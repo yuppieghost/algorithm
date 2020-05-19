7f4cb9cefd02f6baf00a267b9f8d4288cd2469ea
ouzm <wjj315315@gmail.com>
2020-05-19 17:23:07 +0800

    双指针

--- acwing_ex/第一章基础算法/799.最长连续不重复子序列.cpp
+++ acwing_ex/第一章基础算法/799.最长连续不重复子序列.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 
 const int N = 100010;
+
 int n;
 int a[N], s[N];
 
 int main()
 {
   cin >> n;
-  int res = 0;
   for (int i = 0; i < n; i++)
-    scanf("%d", &a[i]);
+    cin >> a[i];
+  int res = 0;
   for (int i = 0, j = 0; i < n; i++)
   {
     s[a[i]]++;
@@ -23,5 +24,4 @@ int main()
     res = max(res, i - j + 1);
   }
   cout << res << endl;
-  return 0;
 }

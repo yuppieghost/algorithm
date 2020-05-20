3414bbd3ab197d54d375d17dc77c381ab771292b
ouzm <wjj315315@gmail.com>
2020-05-20 09:31:41 +0800

    lowbit

--- acwing_ex/第一章basic/801.二进制中1的个数.cpp
+++ acwing_ex/第一章basic/801.二进制中1的个数.cpp
@@ -14,11 +14,15 @@ int main()
 {
   int n;
   cin >> n;
-  for (int i = 0; i < n; i++)
-    scanf("%d", &a[i]);
-  for (int i = 0; i < n; i++)
+
+  while (n--)
   {
+    int x;
+    cin >> x;
     int res = 0;
-    
+    while (x)
+      x -= lowbit(x), res++;
+    cout << res << ' ';
   }
+  return 0;
 }
